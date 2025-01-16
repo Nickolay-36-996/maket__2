5e9fc4b5921614c2a3deb069b66100285f6901d3
viperr <Antonostr2004@gmail.com>
2025-01-16 22:00:49 +0300

    message

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// // const button = document.querySelector(".entered__to__account")
+// // const title = document.querySelector(".blue")
+// // console.log(button);
+// // button.addEventListener("click",function(){
+// //     title.style.color = "red"
+// // })
+
+// const link = document.getElementsByTagName("a")
+// const title = document.getElementById("blue")
+// console.log(link)
+// link.addEventListener("click", function(){
+//     title.style.color = "red"
+// })
+
+const burger = document.querySelector(".burger__menu")
+burger.addEventListener("click",function(){
+    burger.classList.toggle("burger__active")
+})
