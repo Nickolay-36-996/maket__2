8160dcd5b70c88464880e27e324ba2109a5fc263
viperr <Antonostr2004@gmail.com>
2025-01-23 21:01:16 +0300

    message

--- js/index.js
+++ js/index.js
@@ -1,29 +1,6 @@
-// // const button = document.querySelector(".entered__to__account")
-// // const title = document.querySelector(".blue")
-// // console.log(button);
-// // button.addEventListener("click",function(){
-// //     title.style.color = "red"
-// // })
-
-// const link = document.getElementsByTagName("a")
-// const title = document.getElementById("blue")
-// console.log(link)
-// link.addEventListener("click", function(){
-//     title.style.color = "red"
-// })
-
 const burger = document.querySelector(".burger__menu")
+const nav = document.querySelector('.nav')
 burger.addEventListener("click",function(){
     burger.classList.toggle("burger__active")
+    nav.classList.toggle("nav__active")
 })
-
-let name = 'John';
-let admin;
-admin = 'John'
-console.log(admin);
-
-let earth;
-let user;
-
-// let Name = prompt ("Your Name?")
-// console.log(Name);
---
+++ js/learn.js
@@ -0,0 +1,7 @@
+'use strict'
+let first = '12'
+console.log(typeof first)
+let B = Number(first)
+console.log(typeof B)
+let age = Number('R')
+console.log(age)
